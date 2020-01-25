74780ee69593bdc2f0b6ce06c54de60965b6d7d4
Justin Liu <justin.xin.liu@gmail.com>
2020-01-25 17:04:55 +0800

    Fixed compile time error.

--- DevDay2020KeynoteDemoUWP/Pages/DetailPage.xaml.cs
+++ DevDay2020KeynoteDemoUWP/Pages/DetailPage.xaml.cs
@@ -1,30 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.UI.WindowManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+using Windows.UI.Xaml.Hosting;
 
 namespace DevDay2020KeynoteDemoUWP.Pages
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class DetailPage : Page
     {
         public DetailPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // 1. Create a new Window
+            var appWindow = await AppWindow.TryCreateAsync();
+
+            // 2. Create the pageand set the new window's content
+            ElementCompositionPreview.SetAppWindowContent(appWindow, new WonderbarPage());
+
+            // 3. Check if you can leverage the compact overlay APIs
+            if (appWindow.Presenter.IsPresentationSupported(AppWindowPresentationKind.CompactOverlay))
+            {
+                // 4. Show the window
+                await appWindow.TryShowAsync();
+
+                // 5. If so, change that window to be inside the compact overlay region
+                appWindow.Presenter.RequestPresentation(AppWindowPresentationKind.CompactOverlay);
+            }
         }
     }
 }
