a1edf8eb8b61f74122f44843f888b4ad95ca6ab8
Justin Liu <justin.xin.liu@gmail.com>
2020-02-03 19:01:08 -0800

    Updated W.

--- DevDay2020KeynoteDemoUWP/Pages/WonderbarPage.xaml.cs
+++ DevDay2020KeynoteDemoUWP/Pages/WonderbarPage.xaml.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using DevDay2020KeynoteDemoUWP.Model;
+﻿using DevDay2020KeynoteDemoUWP.Model;
 using System.Collections.Generic;
 using System.Linq;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Hosting;
-using Windows.UI.WindowManagement;
 
 namespace DevDay2020KeynoteDemoUWP.Pages
 {
@@ -15,15 +11,6 @@ namespace DevDay2020KeynoteDemoUWP.Pages
         public WonderbarPage()
         {
             InitializeComponent();
-        }
-
-        private async void OnWunderbarClick(object sender, RoutedEventArgs e)
-        {
-            var appWindow = await AppWindow.TryCreateAsync();
-            ElementCompositionPreview.SetAppWindowContent(appWindow, new MainPage());
-            await appWindow.TryShowAsync();
-
-            appWindow.Presenter.RequestPresentation(AppWindowPresentationKind.Default);
         }
     }
 }
